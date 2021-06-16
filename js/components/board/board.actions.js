cbfd84f8947489a4bdcd1ea3864df4970459e49a
Adam Grant <adam_grant@apple.com>
2021-06-16 00:46:24 -0700

    We have all the data for the board now.

--- js/components/board/board.actions.js
+++ js/components/board/board.actions.js
@@ -1,4 +1,11 @@
 m.board.acts({
+    load_board_data(_$, args) {
+        _$.act.get_game_record_id()
+            .then(record => _$.act.get_categories(record))
+            .then(categories => _$.act.get_answers(categories))
+            .catch(error => console.error(error));
+    },
+
     get_game_record_id(_$, args) {
         const url_params = new URLSearchParams(window.location.search);
         const game_id = url_params.get("game") || url_params.get("game_id") || url_params.get("game-id");
@@ -43,27 +50,29 @@ m.board.acts({
 
     get_answers(_$, categories) {
         console.log("Requesting Category-Answers...");
-        let promise_array = [];
 
         categories.forEach((category, index) => {
             console.log(`Requesting Answers for Category ${category.name}...`);
             let records = [];
-            promise_array.push(new Promise((resolve, reject) => {
-                m.airtable.act.find_all({
-                    table: "Category-Answers",
-                    record_ids: category.answers,
-                    handle_records: (record_page) => { records = records.concat(record_page) },
-                    handle_error: (error) => { console.error(error) },
-                    done: (records) => { 
-                        console.log(`Found answers for Category ${category.name}: ${records.map(r => r.get("Answer"))}`)
-                        categories[index].answers = records.map(r => r.get("Answer"));
-                    }
-                })
-            }));
+            m.airtable.act.find_all({
+                table: "Category-Answers",
+                record_ids: category.answers,
+                handle_records: (record_page) => { records = records.concat(record_page) },
+                handle_error: (error) => { console.error(error) },
+                done: (records) => { 
+                    console.log(`Found answers for Category ${category.name}: ${records.map(r => r.get("Answer"))}`);
+                    categories[index].answers = categories[index].answers.map((answer, i) => {
+                        const record = records[i];
+                        return {
+                            name: record.get("Answer"),
+                            price: record.get("Price"),
+                            answered: !!record.get("Won by")
+                        }
+                    });
 
-            Promise.allSettled(promise_array).then(data => {
-
-            });
+                    console.log(categories[index]);
+                }
+            })
         });
     }
 });
--- js/components/board/board.events.js
+++ js/components/board/board.events.js
@@ -1,6 +1,3 @@
 m.board.events(_$ => {
-    _$.act.get_game_record_id()
-        .then(record => _$.act.get_categories(record))
-        .then(categories => _$.act.get_answers(categories))
-        .catch(error => console.error(error));
+    _$.act.load_board_data()
 });
