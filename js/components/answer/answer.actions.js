9ae99eade7985887a1a1f57df9ff4a8ed85606b1
Adam Grant <adam_grant@apple.com>
2021-06-16 23:14:16 -0700

    Now we can do some CSS

--- js/components/answer/answer.actions.js
+++ js/components/answer/answer.actions.js
@@ -1,8 +1,9 @@
 m.answer.acts({
     generate_markup(_$, args) {
         return `
-            <div data-component="answer" data-answer="${args.answer}">
-                <div class="header">${args.answer}</div>
+            <div data-component="answer" data-answer="${args.name}">
+                <div class="price">${args.price}</div>
+                <div class="full-text">${args.name}</div>
             </div>
         ` 
     }
