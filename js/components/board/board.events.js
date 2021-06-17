8cc514c853a4c157b94ddcc8b3d3baaa1f8f2a87
Adam Grant <adam_grant@apple.com>
2021-06-17 14:11:36 -0700

    Decent styling for now

--- js/components/board/board.events.js
+++ js/components/board/board.events.js
@@ -3,5 +3,5 @@ const REFRESH_RATE_IN_SECONDS = 5;
 m.board.events(_$ => {
     _$.act.load_board_data()
 
-    setInterval(_$.act.load_board_data, REFRESH_RATE_IN_SECONDS * 1000);
+    // setInterval(_$.act.load_board_data, REFRESH_RATE_IN_SECONDS * 1000);
 });
--- js/components/column/column.actions.js
+++ js/components/column/column.actions.js
@@ -6,7 +6,7 @@ m.column.acts({
     generate_markup(_$, args) {
         return `
             <div data-component="column" data-column="${args.name}">
-                <div class="header">${args.name}</div>
+                <div class="header"><h1>${args.name}<span></div>
             </div>
         `
     },
@@ -14,8 +14,9 @@ m.column.acts({
     add_answers(_$, args) {
         if (_$.act.same_as_cache(args)) return;
         if (!args.answers) return console.error(`Category ${args.name} has no answers`);
+        args.answers = _$.act.sort_answers(args);
         m.column.cache[args.name] = args
-        const markup = _$.act.sort_answers(args)
+        const markup = args.answers
             .map(answer => m.answer.act.generate_markup(answer))
             .join("\n");
         const this_column = _$.act.get_column_by_name(args)
