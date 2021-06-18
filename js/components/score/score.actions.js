a5430ae5d442fe086742044b3a275c0ff62551cb
Adam Grant <adam_grant@apple.com>
2021-06-18 00:15:43 -0700

    Fixing styles

--- js/components/score/score.actions.js
+++ js/components/score/score.actions.js
@@ -1,17 +1,24 @@
 m.score.acts({
   setup(_$, args) {
-    const board = document.querySelector("[data-component~='board']")
-    board.innerHTML += `
-      <div id="scorebox"></div>
-    `
+    const board = document.querySelector("[data-component~='board']");
+
+    if (!m.score.box_set_up) {
+        board.innerHTML += `<div id="scorebox"></div>`;
+        m.score.box_set_up = true;
+    };
+
     const players = _$.act.get_players(args);
     _$.act.add_score_boxes({ players: players });
     return args;
   },
 
   priv: {
+    get_score_box(_$, args) {
+        return document.getElementById("scorebox");
+    },
+
     add_score_boxes(_$, args) {
-        const scorebox = document.getElementById("scorebox");
+        const scorebox = _$.act.get_score_box();
         scorebox.innerHTML = "";
         args.players.forEach(player => {
             const markup = _$.act.generate_score_markup(player);
@@ -38,3 +45,5 @@ m.score.acts({
     }
   }
 });
+
+m.score.box_set_up = false;
